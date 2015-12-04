70955bc36e01f2d3382876b5c323deb176a42c9c
Kapor <song.zhu@qm.gg>
2015-12-04 14:31:44 +0800

    new: added two decorators
    1. @timeout
    2. @trace

---
+++ kutils/decorators.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+import functools
+import linecache
+import os
+import signal
+import sys
+
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds, error_message='Function call timed out'):
+    def decorated(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return functools.wraps(func)(wrapper)
+    return decorated
+
+
+def trace(f):
+    def globaltrace(frame, why, arg):
+        if why == "call":
+            return localtrace
+        return None
+
+    def localtrace(frame, why, arg):
+        if why == "line":
+            # record the file name and line number of every trace
+            filename = frame.f_code.co_filename
+            lineno = frame.f_lineno
+            bname = os.path.basename(filename)
+            print "{}({}): {}".format(bname, lineno, linecache.getline(filename, lineno)),
+        return localtrace
+
+    def _f(*args, **kwds):
+        sys.settrace(globaltrace)
+        result = f(*args, **kwds)
+        sys.settrace(None)
+        return result
+    return _f
